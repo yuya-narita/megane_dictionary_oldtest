e9e752b0dc2ac032733b2ecadabb795111902578
yuya-narita <yuya.narita.research@gmail.com>
2026-08-04 20:41:06 +0900

    Update series.js

--- syntax_frontier/series.js
+++ syntax_frontier/series.js
@@ -2,11 +2,23 @@ window.SHINO_SERIES = {
   id: "syntax-frontier",
   title: "Syntax Frontier",
   subtitle: "人工世界を、観測する。",
-  themeSrc: "./audio/frontier_loop.wav",
+  themeSrc: "./audio/kaze.mp3",
   defaultVolume: 0.16,
-  episodes: [{
-    id:"ep000", number:0, title:"跳ねのない街",
-    description:"世界は、跳ねを一度も起こさない。",
-    src:"./ep/ep000.js"
-  }]
+
+  episodes: [
+    {
+      id:"ep000",
+      number:0,
+      title:"跳ねのない街",
+      description:"世界は、跳ねを一度も起こさない。",
+      src:"./ep/ep000.js"
+    },
+    {
+      id:"ep001",
+      number:1,
+      title:"声のそろう通り",
+      description:"今日もいい天気ですね",
+      src:"./ep/ep001.js"
+    }
+  ]
 };
