8fe27bb318207cafec8723dd33db6dc5cdb8a63f
yuya-narita <yuya.narita.research@gmail.com>
2026-08-05 23:11:53 +0900

    Add files via upload

---
+++ hx/series.js
@@ -0,0 +1,212 @@
+window.SHINO_SERIES = {
+  "id": "hx-infinity-origin",
+  "title": "H(x)∞ origin",
+  "subtitle": "世界バグ報告書：NIX_LOG_∞",
+  "themeSrc": "./audio/tracks/track01.mp3",
+  "defaultVolume": 0.58,
+  "episodes": [
+    {
+      "id": "track01",
+      "number": 1,
+      "title": "Calm Down BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track01.js"
+    },
+    {
+      "id": "track02",
+      "number": 2,
+      "title": "WHITE BEAR",
+      "description": "世界バグ報告書",
+      "src": "./ep/track02.js"
+    },
+    {
+      "id": "track03",
+      "number": 3,
+      "title": "WHY? BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track03.js"
+    },
+    {
+      "id": "track04",
+      "number": 4,
+      "title": "VOICE BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track04.js"
+    },
+    {
+      "id": "track05",
+      "number": 5,
+      "title": "SILENCE BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track05.js"
+    },
+    {
+      "id": "track06",
+      "number": 6,
+      "title": "DOUBLE BIND",
+      "description": "世界バグ報告書",
+      "src": "./ep/track06.js"
+    },
+    {
+      "id": "track07",
+      "number": 7,
+      "title": "COMPARISON BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track07.js"
+    },
+    {
+      "id": "track08",
+      "number": 8,
+      "title": "POSITIVE PRESSURE",
+      "description": "世界バグ報告書",
+      "src": "./ep/track08.js"
+    },
+    {
+      "id": "track09",
+      "number": 9,
+      "title": "AI THANK YOU BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track09.js"
+    },
+    {
+      "id": "track10",
+      "number": 10,
+      "title": "OVER-OBSERVATION",
+      "description": "世界バグ報告書",
+      "src": "./ep/track10.js"
+    },
+    {
+      "id": "track11",
+      "number": 11,
+      "title": "LABELING BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track11.js"
+    },
+    {
+      "id": "track12",
+      "number": 12,
+      "title": "STORY BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track12.js"
+    },
+    {
+      "id": "track13",
+      "number": 13,
+      "title": "BELIEF BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track13.js"
+    },
+    {
+      "id": "track14",
+      "number": 14,
+      "title": "CROWD BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track14.js"
+    },
+    {
+      "id": "track15",
+      "number": 15,
+      "title": "RECURSION BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track15.js"
+    },
+    {
+      "id": "track16",
+      "number": 16,
+      "title": "EXPECTATION BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track16.js"
+    },
+    {
+      "id": "track17",
+      "number": 17,
+      "title": "APPROVAL BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track17.js"
+    },
+    {
+      "id": "track18",
+      "number": 18,
+      "title": "PROHIBITION BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track18.js"
+    },
+    {
+      "id": "track19",
+      "number": 19,
+      "title": "REASON BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track19.js"
+    },
+    {
+      "id": "track20",
+      "number": 20,
+      "title": "MEMORY BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track20.js"
+    },
+    {
+      "id": "track21",
+      "number": 21,
+      "title": "NORMAL BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track21.js"
+    },
+    {
+      "id": "track22",
+      "number": 22,
+      "title": "JUSTICE BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track22.js"
+    },
+    {
+      "id": "track23",
+      "number": 23,
+      "title": "FREEDOM BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track23.js"
+    },
+    {
+      "id": "track24",
+      "number": 24,
+      "title": "CHOICE BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track24.js"
+    },
+    {
+      "id": "track25",
+      "number": 25,
+      "title": "RESPONSIBILITY BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track25.js"
+    },
+    {
+      "id": "track26",
+      "number": 26,
+      "title": "TRUST BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track26.js"
+    },
+    {
+      "id": "track27",
+      "number": 27,
+      "title": "PROMISE BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track27.js"
+    },
+    {
+      "id": "track28",
+      "number": 28,
+      "title": "LOVE BUG",
+      "description": "世界バグ報告書",
+      "src": "./ep/track28.js"
+    },
+    {
+      "id": "track29",
+      "number": 29,
+      "title": "THE OBSERVER",
+      "description": "世界バグ報告書",
+      "src": "./ep/track29.js"
+    }
+  ]
+};
