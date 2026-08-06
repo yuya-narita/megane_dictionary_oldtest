1e740324251614730d75b4a461926c0a99f4644f
yuya-narita <yuya.narita.research@gmail.com>
2026-08-06 23:16:23 +0900

    Update track01.js

--- hx/ep/track01.js
+++ hx/ep/track01.js
@@ -101,7 +101,7 @@ window.SHINO_EPISODES["ep001"] = {
       "ambience": {
         "action": "start",
         "src": "./audio/hx_000/track01.mp3",
-        "volume": 0.5,
+        "volume": 0.1,
         "fadeIn": 0,
         "fadeOut": 235000,
         "loop": true,
