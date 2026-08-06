52e195c924556800efa5a3c39159ff4ab298fbdd
yuya-narita <yuya.narita.research@gmail.com>
2026-08-07 01:00:23 +0900

    Update track01.js

--- top/ep/track01.js
+++ top/ep/track01.js
@@ -1,6 +1,6 @@
 window.SHINO_EPISODES = window.SHINO_EPISODES || {};
-window.SHINO_EPISODES["ep001"] = {
-  "id": "ep001",
+window.SHINO_EPISODES["track01"] = {
+  "id": "track01",
   "number": 1,
   "title": "世界バグ報告書：NIX_LOG_∞",
   "description": "",
@@ -101,8 +101,8 @@ window.SHINO_EPISODES["ep001"] = {
       "ambience": {
         "action": "start",
         "src": "./audio/hx_000/track01.mp3",
-        "volume": 0.5,
-        "fadeIn": 1000,
+        "volume": 0.1,
+        "fadeIn": 1500,
         "fadeOut": 1500,
         "loop": true,
         "restart": true
